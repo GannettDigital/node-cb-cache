c3d2c4bf394cd84a652dccfb900a238171ac16e2
Eric Moritz <ericmoritz@gmail.com>
2015-04-10 12:34:07 -0400

    some compat fixes

--- cb-cache.js
+++ cb-cache.js
@@ -8,6 +8,9 @@ function CBCache(bucket) {
   this._bucket = bucket;
 }
 
+CBCache.prototype.flush = function(callback) {
+  this._bucket.manager().flush(callback);
+}
 
 CBCache.prototype.get = function(key, callback) {
   this._bucket.get(key, function(err, obj) {
@@ -20,10 +23,18 @@ CBCache.prototype.get = function(key, callback) {
 
 
 CBCache.prototype.set = function(key, value, lifetime, callback) {
+  var options = {}
+  // If the callback is a function, lifetime should be a timeout
+  if (typeof callback === 'function') {
+    options = {'expiry': lifetime};
+  // otherwise lifetime is the callback
+  } else {
+      callback = lifetime;
+  }
   this._bucket.upsert(
     key,
     value,
-    {'expiry': lifetime},
+    options,
     callback
   )
 }
@@ -34,6 +45,7 @@ CBCache.prototype.del = function(key, callback) {
     filter_keyNotFound(err, undefined, callback);
   });
 }
+
 
 /*******************************************************************************
 ** Internal
